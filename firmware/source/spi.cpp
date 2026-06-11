db5cd86b67356679535e079bf838f55aa260207e
eric <ericaustin2005@gmail.com>
2026-06-11 20:08:53 +0100

    prototype microbit SPI function

--- firmware/source/spi.cpp
+++ firmware/source/spi.cpp
@@ -8,11 +8,25 @@ void cnf_spi_pins(){
     NRF_SPIM0->PSEL.SCK  = 13; // telling SPI peripheral to send clock to 13?
     NRF_SPIM0->PSEL.MISO = 0xFFFFFFFF;  // blanking MISO for now
     NRF_SPIM0->PSEL.MOSI = 15; // telling SPI peripheral to send over pin 15
+    NRF_SPIM0->PSEL.CSN = 16; // lets send CS over 16 as well so its easier to implement the slave
+    NRF_SPIM0->FREQUENCY = 0x80000000; // frequency to 8MHz
+    NRF_SPIM0 -> CONFIG = 0b000;
 }
 
-void send_arr(uint32_t *arr, uint32_t size){
+void send_arr(uint8_t *arr, uint32_t size){
+    cnf_spi_pins();
+    NRF_SPIM0->EVENTS_END = 0;
 
-    NRF_SPIM0->TXD.PTR = uint32_t(arr);
-    NRF_SPIM0->TXD.MAXCNT = size; 
+    NRF_SPIM0->TXD.PTR = (uint32_t)arr;
+    NRF_SPIM0->TXD.MAXCNT = (size); 
+    
+    NRF_SPIM0->ENABLE = 0b111;
 
+    NRF_SPIM0->TASKS_START = 1;
+
+    while(NRF_SPIM0->EVENTS_END==0);
+
+    NRF_SPIM0->TASKS_STOP = 1;
+    
+    NRF_SPIM0->ENABLE = 0;
 }
