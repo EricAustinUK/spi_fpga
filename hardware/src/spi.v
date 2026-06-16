a2911bb105a7cedd2ff3cec5c9bf76e5a881b833
eric <ericaustin2005@gmail.com>
2026-06-16 16:38:31 +0100

    fixed tang nano using improper pin for SPI clock signal

--- hardware/src/spi.v
+++ hardware/src/spi.v
@@ -2,12 +2,14 @@ module spi_recv(
     input i_clk,
     input i_mosi,
     input i_cs,
-    output reg [1:0] o_debugrow
+    output wire [1:0] o_debugrow
 );
 
 reg [31:0] mem_br;
 reg [27:0] img_mem [0:27];
 reg [4:0] row = 5'd0;
+
+assign o_debugrow = mem_br[1:0];
 
 always @(posedge i_clk) begin
     if (!i_cs) begin
@@ -17,7 +19,6 @@ end
 
 always @(posedge i_cs) begin
     img_mem[row] <= mem_br[27:0];
-    o_debugrow <= mem_br[1:0];
     row <= row + 5'd1;
 end
 
