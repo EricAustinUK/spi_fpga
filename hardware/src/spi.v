da3e7eb314f57bd7419b320692caadf8c25fdfe3
eric <ericaustin2005@gmail.com>
2026-06-17 10:56:07 +0100

    patches to SPI slave to increase stabilty (previously was treating chip select as a clock which the compiler was warning me about, so manually implemented rising and falling edge). Also changed pulseview file extensions

--- hardware/src/spi.v
+++ hardware/src/spi.v
@@ -1,25 +1,30 @@
 module spi_recv(
     input i_clk,
+    input i_sck,
     input i_mosi,
     input i_cs,
-    output wire [1:0] o_debugrow
+    output reg [27:0] o_row_bits,
+    output reg o_data_ready
 );
 
 reg [31:0] mem_br;
-reg [27:0] img_mem [0:27];
-reg [4:0] row = 5'd0;
 
-assign o_debugrow = mem_br[1:0];
-
-always @(posedge i_clk) begin
+always @(posedge i_sck) begin
     if (!i_cs) begin
         mem_br <= {mem_br[30:0], i_mosi};
     end
 end
 
-always @(posedge i_cs) begin
-    img_mem[row] <= mem_br[27:0];
-    row <= row + 5'd1;
+reg prev_cs = 1;
+
+always @(posedge i_clk) begin
+    if (prev_cs == 0 && i_cs == 1) begin 
+        o_row_bits <= mem_br[27:0];
+        o_data_ready <= 1;
+    end else if(prev_cs == 1 && i_cs == 0) begin
+        o_data_ready <= 0;
+    end
+    prev_cs <= i_cs;
 end
 
 endmodule
