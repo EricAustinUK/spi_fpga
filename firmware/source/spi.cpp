95b85561fc1492bc02eab74669acd04057b30b78
eric <ericaustin2005@gmail.com>
2026-06-28 13:37:17 +0100

    Finally works. Reverted to what was giving me pos vsync bits before and did my spi r/w fixes, and it finally captures frames!!

--- firmware/source/spi.cpp
+++ firmware/source/spi.cpp
@@ -4,25 +4,6 @@
 #define CAM_CSN 9
 #define NANO_CSN 4
 
-void slp_100ms(){
-    NRF_TIMER2->TASKS_STOP = 1; // stop other timers
-    NRF_TIMER2->BITMODE = 3; // 32 bit
-    NRF_TIMER2->CC[0] = 100000; // set cmp to time
-    NRF_TIMER2->PRESCALER = 4;
-
-    // clear flags
-    NRF_TIMER2->EVENTS_COMPARE[0] = 0;
-    NRF_TIMER2->TASKS_CLEAR = 1;
-
-    // wait for delay to be met
-    NRF_TIMER2->TASKS_START = 1;
-    while(NRF_TIMER2->EVENTS_COMPARE[0] == 0);
-    
-    // clear flags
-    NRF_TIMER2->TASKS_STOP = 1;
-    NRF_TIMER2->TASKS_CLEAR = 1;
-    NRF_TIMER2->EVENTS_COMPARE[0] = 0;
-}
 
 void cnf_spi_pins(){
     static volatile bool cfgd = false;
@@ -64,14 +45,14 @@ void spi_send_arr(volatile uint32_t arr_ptr, uint32_t size, bool is_to_nano){
     
     NRF_SPIM3->TASKS_START = 1; // start sending array
 
-    while(NRF_SPIM3->EVENTS_END==0); // wait for event to end
+    while(NRF_SPIM3->EVENTS_END==0); // busy wait for event to end
 
     NRF_P0->OUTSET = (1 << (is_to_nano ? NANO_CSN : CAM_CSN)); // set CS high
 
     NRF_SPIM3->TASKS_STOP = 1; // stop sending when buffer is sent
 }
 
-bool send_i2c_cmd(volatile sensor_reg * data_ptr){
+void send_i2c_cmd(volatile sensor_reg * data_ptr){
     NRF_TWIM0->TXD.MAXCNT = 2; // command is two instructions in memory
     NRF_TWIM0->TXD.PTR = (uint32_t) data_ptr; // set pointer to command
     NRF_TWIM0->EVENTS_STOPPED = 0; // reset flag
@@ -81,29 +62,13 @@ bool send_i2c_cmd(volatile sensor_reg * data_ptr){
 
     NRF_TWIM0->TASKS_STARTTX = 1; // start transfer over SCCB
 
-    while(NRF_TWIM0->EVENTS_STOPPED==0); // no longer a busy wait... if i knew it was this easy id have done this from the start!
-
-    if(NRF_TWIM0->EVENTS_ERROR){
-        NRF_TWIM0->EVENTS_ERROR = 0;
-        NRF_TWIM0->ERRORSRC = NRF_TWIM0->ERRORSRC; // cool refactor: errors do 1 to clear, so writing the current error ONLY clears the flags for that error 
-        return false;
-    }
-
-    return true;
+    while(NRF_TWIM0->EVENTS_STOPPED==0); // busy wait for stop
 }
 
 void cnf_camera(){
     static volatile bool cfgd = false;
     if(cfgd) return;
     cfgd = true;
-    
-    volatile sensor_reg cpld_reset = {0x87, 0x80};
-    spi_send_arr(uint32_t (&cpld_reset), 2, false);
-    slp_100ms(); 
-
-    volatile sensor_reg clear_reset = {0x87, 0x00};
-    spi_send_arr(uint32_t (&clear_reset), 2, false);
-    slp_100ms();
 
     NRF_TWIM0->PSEL.SCL = 26; // set i2c scl to external pin 20
     NRF_TWIM0->PSEL.SDA = 32; // set i2c sda to external pin 19
@@ -111,37 +76,14 @@ void cnf_camera(){
     NRF_TWIM0->ENABLE = 0b110; // enable twim peripheral
     NRF_TWIM0->ADDRESS = 0x30; // set to arducams address
 
-    volatile sensor_reg select_sensor_bank = { 0xFF, 0x01 };
-    volatile sensor_reg reset = { 0x12, 0x80 }; // must send reset for some reason!
-
-    send_i2c_cmd(&select_sensor_bank);
-    send_i2c_cmd(&reset);
-
-    slp_100ms();
-
     for(uint32_t i = 0; !(OV2640_JPEG_INIT[i].reg == 0xFF && OV2640_JPEG_INIT[i].val == 0xFF); i++){
         volatile sensor_reg * row_ptr = &OV2640_JPEG_INIT[i];
         send_i2c_cmd(row_ptr);
-    }
+    }    
 
-    for(uint32_t i = 0; !(OV2640_YUV422[i].reg == 0xFF && OV2640_YUV422[i].val == 0xFF); i++){
-        volatile sensor_reg * row_ptr = &OV2640_YUV422[i];
-        send_i2c_cmd(row_ptr);
-    }
-
-    for(uint32_t i = 0; !(OV2640_JPEG[i].reg == 0xFF && OV2640_JPEG[i].val == 0xFF); i++){
-        volatile sensor_reg * row_ptr = &OV2640_JPEG[i];
-        send_i2c_cmd(row_ptr);
-    }
-
-    volatile sensor_reg vsync_fix = {0x15, 0x00};
-
-    send_i2c_cmd(&select_sensor_bank); // Ensure we are in the sensor bank
-    send_i2c_cmd(&vsync_fix);
-
-    for(uint32_t i = 0; !(OV2640_320x240_JPEG[i].reg == 0xFF && OV2640_320x240_JPEG[i].val == 0xFF); i++){
-        volatile sensor_reg * row_ptr = &OV2640_320x240_JPEG[i];
-        send_i2c_cmd(row_ptr);
+    for(uint32_t i = 0; !(OV2640_640x480_JPEG[i].reg == 0xFF && OV2640_640x480_JPEG[i].val == 0xFF); i++){
+        volatile sensor_reg * row_ptr = &OV2640_640x480_JPEG[i];
+        send_i2c_cmd(row_ptr); 
     }
 
     NRF_TWIM0->ENABLE = 0; // disable twim peripheral
@@ -153,10 +95,9 @@ void write_cam_reg(sensor_reg reg){
 }
 
 uint8_t read_cam_reg(uint8_t reg){
-    reg &= 0x7F;
     // clear event registers
     NRF_SPIM3->EVENTS_END = 0;
-    volatile uint8_t send_packet = reg | (0x00 << 8);
+    volatile uint16_t send_packet = reg | (0x00 << 8);
     // configure TX buffer
     NRF_SPIM3->TXD.PTR = (uint32_t) &send_packet; 
     NRF_SPIM3->TXD.MAXCNT = 2; 
@@ -170,11 +111,11 @@ uint8_t read_cam_reg(uint8_t reg){
     
     NRF_SPIM3->TASKS_START = 1; // start sending array
 
-    while(NRF_SPIM3->EVENTS_END==0); // wait for end of rxd and txd buffers
+    while(NRF_SPIM3->EVENTS_END==0); // busy wait for end of rxd and txd buffers
 
     NRF_SPIM3->TASKS_STOP = 1; // stop when both happen
 
-    NRF_P0->OUTSET = (1 << CAM_CSN); // set CS high 
+    NRF_P0->OUTSET = (1 << CAM_CSN); // set CS high
     
     return value >> 8;
 }
@@ -182,7 +123,7 @@ uint8_t read_cam_reg(uint8_t reg){
 void get_img(){
     cnf_spi_pins();
 
-    write_cam_reg({ 0x04, 0x01 }); // ONLY clear the FIFO flag
+    write_cam_reg({ 0x04, 0b00110001 });  // clear FIFO and reset r/w pointers
     write_cam_reg({ 0x04, 0x02 });  // start capture
 
     while(!(read_cam_reg(0x41) & 0x08)); // 0x41 is the trigger register and 0x08 is the bitmask for the capture done flag, with 0x01 for vsync going low
